0dc2f611d2d95b8032e143b044dc9fd7ffd96f27
Geovanny251201 <69869435+Geovanny251201@users.noreply.github.com>
2023-05-13 00:42:51 -0600

    arreglo de fallos

--- ProyectoPersonal/estudiantesMain.js
+++ ProyectoPersonal/estudiantesMain.js
@@ -1,149 +1,125 @@
-// var formulario = document.getElementById("form-estudiantes");
 
-// formulario.addEventListener("submit", function (e) {
-
-//   e.preventDefault();
-//   var datosFormulario;
-//   datosformulario = new FormData(formulario);
-
-//   var formularioLleno = true;
-//   for (var entrada of datosformulario.entries()) {
-//     if (!entrada[1]) { // si el valor de la entrada está vacío
-//       formularioLleno = false;
-//       break;
-//     }
-//   }
-
-// if(formularioLleno){
-//     fetch("https://paginas-web-cr.com/ApiPHP/apis/InsertarEstudiantes.php", {
-//         method: "POST",
-//         body: JSON.stringify(Object.fromEntries(datosformulario.entries())),
-//       })
-//         .then((respuesta) => respuesta.json()) //recibe los datos em formato json
-//         .then((datosrespuesta) => {
-//         // Cerrar el modal
-//         $('#exampleModal').modal('hide');
-//         // Vaciar los inputs
-//         formulario.reset(); 
-
-//         })
-//         .catch(console.log);
-//         Swal.fire({
-//             icon: 'success',
-//             title: 'Guardado',
-//             text: 'Estudiante Agregado',
-//             confirmButtonColor: '#4fb37c',
-//           }
-//           )
-// }else{
-//      Swal.fire({
-//         icon: 'error',
-//         title: 'Oops...',
-//         text: 'Por favor llene todos los campos',
-//         confirmButtonColor: '#dc3545',
-//       });
-// }
-
-// });
-
-var campoCedula = document.querySelector("#cedula");
-var campoCorreoElectronico = document.querySelector("#correoelectronico");
-var campoTelefono = document.querySelector("#telefono");
-var campoTelefonoCelular = document.querySelector("#telefonocelular");
-var campoFechaNacimiento = document.querySelector("#fechanacimiento");
-var campoSexo = document.querySelector("#sexo");
-var campoDireccion = document.querySelector("#direccion");
-var campoNombre = document.querySelector("#nombre");
-var campoApellidopaterno = document.querySelector("#apellidopaterno");
-var campoApellidomaterno = document.querySelector("#apellidomaterno");
-var campoNacionalidad = document.querySelector("#nacionalidad");
-var campoIdCarreras = document.querySelector("#idCarreras");
-var campoUsuario = document.querySelector("#usuario");
-
-
-
-// {"id":"314","cedula":"3465435","correoelectronico":"ge@gmail.com","telefono":"12321","telefonocelular":"2354234",
-// "fechanacimiento":"2023-05-05","sexo":"masculino","direccion":"San Ramon",
-// "nombre":"W2","apellidopaterno":"Ara","apellidomaterno":"Ji","nacionalidad":"Tico","idCarreras":"1","usuario":"jaja"}
-function llenarForm(id, cedula, correoelectronico, telefono, telefonocelular,
+var idValue;
+function llenarForm( id, cedula, correoelectronico, telefono, telefonocelular,
   fechanacimiento, sexo, direccion, nombre, apellidopaterno, apellidomaterno, nacionalidad, idCarreras, usuario) {
+    idValue=id;
+    document.getElementById('cedulaEdit').value=cedula
+    document.getElementById('correoelectronicoEdit').value=correoelectronico
+    document.getElementById('telefonoEdit').value=telefono
+    document.getElementById('telefonocelularEdit').value=telefonocelular
+    document.getElementById('fechanacimientoEdit').value=fechanacimiento
+    document.getElementById('sexoEdit').value=sexo
+    document.getElementById('direccionEdit').value=direccion
+    document.getElementById('nombreEdit').value=nombre
+    document.getElementById('apellidopaternoEdit').value=apellidopaterno
+    document.getElementById('apellidomaternoEdit').value=apellidomaterno
+    document.getElementById('nacionalidadEdit').value=nacionalidad
+    document.getElementById('idCarrerasEdit').value=idCarreras
+    document.getElementById('usuarioEdit').value=usuario
 
-  campoCedula = cedula,
-  campoCorreoElectronico = correoelectronico,
-  campoTelefono = telefono,
-  campoTelefonoCelular = telefonocelular,
-  campoFechaNacimiento = fechanacimiento,
-  campoSexo = sexo,
-  campoDireccion = direccion,
-  campoNombre = nombre
-  campoApellidopaterno = apellidopaterno,
-  campoApellidomaterno = apellidomaterno,
-  campoNacionalidad = nacionalidad,
-  campoIdCarreras = idCarreras,
-  campoUsuario = usuario
   }
-  function editarDatos(id, cedula, correoelectronico, telefono, telefonocelular,
-    fechanacimiento, sexo, direccion, nombre, apellidopaterno, apellidomaterno, nacionalidad, idCarreras, usuario){
 
   var formulario = document.getElementById("form-estudiantes-editar");
+  formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+ 
+   var id =idValue;
+   var cedula = document.getElementById('cedulaEdit').value;
+   var correoelectronico = document.getElementById('correoelectronicoEdit').value;
+   var telefono = document.getElementById('telefonoEdit').value;
+   var telefonocelular = document.getElementById('telefonocelularEdit').value;
+   var fechanacimiento = document.getElementById('fechanacimientoEdit').value;
+   var sexo = document.getElementById('sexoEdit').value;
+   var direccion = document.getElementById('direccionEdit').value;
+   var nombre = document.getElementById('nombreEdit').value;
+   var apellidopaterno = document.getElementById('apellidopaternoEdit').value;
+   var apellidomaterno = document.getElementById('apellidomaternoEdit').value;
+   var nacionalidad = document.getElementById('nacionalidadEdit').value;
+   var idCarreras = document.getElementById('idCarrerasEdit').value;
+   var usuario = document.getElementById('usuarioEdit').value;
 
-  var datosFormulario;
-  datosFormulario = new FormData(formulario);
 
-  var formularioLleno = true;
-  for (var entrada of datosFormulario.entries()) {
-    if (!entrada[1]) { // si el valor de la entrada está vacío
-      formularioLleno = false;
-      break;
-    }
-  }
-  if (formularioLleno) {
+
+
     var datosActualizados = {
-      id: id,
-      cedula:campoCedula,
-      correoelectronico:campoCorreoElectronico,
-      telefono:campoTelefono,
-      telefonocelular:campoTelefonoCelular,
-      fechanacimiento:campoFechaNacimiento,
-      sexo:campoSexo,
-      direccion:campoDireccion,
-      nombre:campoNombre,
-      apellidopaterno:campoApellidopaterno,
-      apellidomaterno:campoApellidomaterno,
-      nacionalidad:campoNacionalidad,
-      idCarreras:campoIdCarreras,
-      usuario:campoUsuario
+        id: id,
+        cedula:cedula,
+        correoelectronico:correoelectronico,
+        telefono:telefono,
+        telefonocelular:telefonocelular,
+        fechanacimiento:fechanacimiento,
+        sexo:sexo,
+        direccion:direccion,
+        nombre:nombre,
+        apellidopaterno:apellidopaterno,
+        apellidomaterno:apellidomaterno,
+        nacionalidad:nacionalidad,
+        idCarreras:idCarreras,
+        usuario:usuario
+    }
+    function validarDatos(datos) {
+      for (var key in datos) {
+        if (datos.hasOwnProperty(key)) {
+          if (!datos[key] || datos[key].trim() === "") {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
 
-    };
 
-    fetch("https://paginas-web-cr.com/ApiPHP/apis/ActualizarEstudiantes.php", {
-        method: "POST",
-        body: JSON.stringify(datosActualizados)
+    if (validarDatos(datosActualizados)) {
+    // console.log(datosenviar);
+    fetch("https://paginas-web-cr.com/ApiPHP/apis/ActualizarEstudiantes.php",
+    {
+        method:"POST",
+        body:JSON.stringify(datosActualizados)
+    })//url de peticion de datos
+    .then(respuesta => respuesta.json())//recibe los datos en formato json
+    .then((datosrespuesta) => {    
+      Swal.fire({
+        icon: 'success',
+        title: 'Guardado',
+        text: 'Estudiante Agregado',
+        confirmButtonColor: '#4fb37c',
       })
-      .then(function (respuesta) {
-        return respuesta.json();
-      })
-      .then(function (datosrespuesta) {
-
-        alert("Los datos se han editado");
-        formulario.reset(); // Restablecer campos del formulario
-        $('#exampleModal').modal('hide');
+        $('#modalEdit').modal('hide');
         cargarDatos();
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-  } else {
-    alert("Campos vacíos");
+        
+    })
+    .catch(console.log)//muestra errores
+  }else{
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Por favor llene todos los campos',
+      confirmButtonColor: '#dc3545',
+    });
   }
+});
 
 
-
+function borrarDatos(id, nombre) {
+  // Mostrar SweetAlert para confirmar la eliminación
+  swal({
+    title: 'Are you sure?',
+    text: "It will permanently deleted !",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then(function() {
+    swal(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    );
+  })
 }
 
-
-
+  // ------------------------------------------------------------
 function agregarDatos() {
   var formulario = document.getElementById("form-estudiantes");
 
